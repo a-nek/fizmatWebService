3545c230785ead82aec0f03cce6786014ecfe7cd
a-nek <forze.e@yandex.by>
2018-02-27 20:50:49 +0300

    added X

---
+++ src/webApplication/mathematicalModel/x/X2.java
@@ -0,0 +1,10 @@
+package webApplication.mathematicalModel.x;
+
+import webApplication.mathematicalModel.X;
+
+public class X2 implements X {
+    @Override
+    public double getX(double s) {
+        return s/2;
+    }
+}
